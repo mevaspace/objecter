398e1dca82a9fef737886b1f44888a23c9c2a109
haloivanid <ivanazis.id@gmail.com>
2026-02-09 01:25:59 +0800

    ⚙️ ci(release): fix semantic release config

---
+++ release.config.js
@@ -0,0 +1,28 @@
+const headerPattern =
+  /^(?:[\u2300-\u23FF]|[\u2B05-\u2B07]|[\u2194-\u2199]|[\u2700-\u27BF]|[\u1F000-\u1F9FF]|[\u1F600-\u1F64F]|[\u1F680-\u1F6FF]|[\u1F300-\u1F5FF])\s(\w+)(?:\(([\w$.\-* ]+)\))?: (.*)$/;
+
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      { preset: 'angular', parserOpts: { headerPattern, headerCorrespondence: ['type', 'scope', 'subject'] } },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      { preset: 'angular', parserOpts: { headerPattern, headerCorrespondence: ['type', 'scope', 'subject'] } },
+    ],
+    '@semantic-release/changelog',
+    ['@semantic-release/npm', { npmPublish: false }],
+    ['@semantic-release/exec', { prepareCmd: 'pnpm build' }],
+    ['@semantic-release/npm', { pkgRoot: 'dist' }],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
